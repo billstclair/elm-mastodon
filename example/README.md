2492255d51bc7ebcae8068c370f5acecf8111117
Bill St. Clair <billstclair@gmail.com>
2019-06-29 07:41:07 -0400

    Add example directory, starting with the elm-oauth-middleware example.
    It builds, but I haven't tried running it yet.

---
+++ example/README.md
@@ -0,0 +1,53 @@
+This directory contains an example of using [`billstclair/elm-mastodon`](http://package.elm-lang.org/packages/billstclair/elm-mastodon/latest) package. To run the example, you first need to run a server, at a `redirectUri` that you've registered with an OAuth provider for the OAuth Authorization Code Grant Flow.
+
+First, clone the code on your computer:
+
+    cd ...
+    git clone git@github.com:billstclair/elm-oauth-middleware.git
+    cd elm-oauth-middleware/example
+
+Learn how to configure and run the server in the [server](../server/) directory's README. Part of doing that will be to create an Authorization Code Grant Flow account with an OAuth provider. I have tested this code with mastodon.social, pleroma.social, and develop.gab.com, but it should work with any Mastodon or Pleroma instance that supports Authorization Code Grant Flow.
+
+If you will run the example only on a web server at a domain configured for your server, you're golden. To run from your development machine, you have to invent an unused domain name, enter it in the `redirectBackHosts` in the server's `config.json` file, add it to `/etc/hosts` on your development machine, [as instructed](https://github.com/billstclair/elm-oauth-middleware#development) in the top-level README, and use it in the `elm-reactor` startup below, where I'm calling it `oauth-client-dev.com`.
+
+Next, you need to create an API configuration file for the example. Copy [`authorizations.json.template`](authorizations.json.template) to `authorizations.json`, and edit that file to contain your OAuth server(s) information.
+
+    [ { "comment": "Any object containing one of more 'comment' properites is ignored."
+        "comment": "To use one of the settings below, remove the"
+        "comment": "'comment' line and fill in 'clientId' and 'redirectUri'.",
+      },
+      { "name": "OAuth Provider Name",
+        "authorizationUri" : "https://example.com/outh/authorize",
+        "tokenUri" : "https://example.com/oauth/access_token",
+        "clientId" : "The client Id assigned to you by the OAuth Provider",
+        "redirectUri" : "The redirect (callback) Uri you provided for that client Id",
+        "scopes" : {"scope name 1": "scope 1",
+                    "scope name 2": "scope 2"
+                   }
+      }
+    ]
+
+If there is a `comment` property in an object in the list, that entire object will be ignored. This allows you to comment your settings, and also allows commenting out an individual setting that you're not using. The provided `authorizations.json.template` contains commented-out real settings that you can use by removing the `comment` property, and filling in your `clientId` and `redirectUri`.
+
+`name` appears in the example's "Provider" selector on the example web page.
+
+`authorizationUri` is for the authorization provider. For mastodon.social, it is `https://github.com/login/oauth/authorize`.
+
+`tokenUri` is also for the authorization provider. For mastodon.social, it is `https://github.com/login/oauth/access_token`.
+
+`clientId` is the identifier assigned by the provider to your OAuth application. It is sort of your OAuth user name. Note that the client SECRET is NOT here. It is only on your token server, so that it will never be seen in a user's browser.
+
+`redirectUri` is the web address of your server. It must exactly match the Uri registered with your OAuth provider.
+
+`scopes` is a list of `name/scope` pairs, encoded as a JSON object. the `scope` part of those pairs is sent to the `authorizationUri` to request particular access permissions. The example currently uses only the first scope it encounters, so entering more than one isn't useful. I will probably eventually let you pick which scope(s) to use.
+
+Now you can run the example with `elm-reactor`:
+
+    cd .../elm-oauth-middleware/example
+    elm reactor -a oauth-client-dev.com
+    
+Aim your browser at http://oauth-client-dev.com:8000/example.elm
+
+You can select a "Provider", and click "Login" to authenticate and get a token from your callback server. Then you can click "Get User" to send an API request for user information.
+
+If you use a provider other than the four I put in `authorizations.json.template`, you'll have to add a `getUser` setting for your provider to the `apis` constant in `example.elm`.
---
+++ example/src/Main.elm
@@ -0,0 +1,411 @@
+----------------------------------------------------------------------
+--
+-- Main.elm
+-- Example of using billstclair/elm-mastodon
+-- Copyright (c) 2019 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE
+--
+----------------------------------------------------------------------
+
+
+module Main exposing (Api, Model, Msg(..), apis, getUser, init, lookupProvider, main, providerOption, providerSelect, update, userAgentHeader, view)
+
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation as Navigation exposing (Key)
+import Dict exposing (Dict)
+import Html
+    exposing
+        ( Attribute
+        , Html
+        , button
+        , div
+        , h2
+        , option
+        , p
+        , pre
+        , select
+        , text
+        )
+import Html.Attributes
+    exposing
+        ( selected
+        , style
+        , value
+        )
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as JD
+import Json.Encode as JE exposing (Value)
+import OAuthMiddleware
+    exposing
+        ( Authorization
+        , ResponseToken
+        , TokenAuthorization
+        , TokenState(..)
+        , authorize
+        , getAuthorizations
+        , locationToRedirectBackUri
+        , receiveTokenAndState
+        , use
+        )
+import OAuthMiddleware.EncodeDecode
+    exposing
+        ( authorizationsEncoder
+        , responseTokenEncoder
+        )
+import Url exposing (Url)
+
+
+type alias Model =
+    { key : Key
+    , authorization : Maybe Authorization
+    , token : Maybe ResponseToken
+    , state : Maybe String
+    , msg : Maybe String
+    , replyType : String
+    , reply : Maybe Value
+    , redirectBackUri : String
+    , provider : String
+    , authorizations : Dict String Authorization
+    , tokenAuthorization : Maybe TokenAuthorization
+    , api : Maybe Api
+    }
+
+
+type Msg
+    = OnUrlRequest UrlRequest
+    | OnUrlChange Url
+    | ReceiveAuthorizations (Result Http.Error (List Authorization))
+    | ChangeProvider String
+    | Login
+    | GetUser
+    | ReceiveUser (Result Http.Error Value)
+
+
+{-| GitHub requires the "User-Agent" header.
+-}
+userAgentHeader : Http.Header
+userAgentHeader =
+    Http.header "User-Agent" "Xossbow"
+
+
+type alias Api =
+    { getUser : String
+    }
+
+
+apis : Dict String Api
+apis =
+    Dict.fromList
+        [ ( "GitHub"
+          , { getUser = "user"
+            }
+          )
+        , ( "Gmail"
+          , { getUser = "me/profile"
+            }
+          )
+        , ( "Facebook"
+          , { getUser = "me"
+            }
+          )
+        , ( "Gab"
+          , { getUser = "users/Xossbow" -- "me/"
+            }
+          )
+        ]
+
+
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , onUrlRequest = OnUrlRequest
+        , onUrlChange = OnUrlChange
+        }
+
+
+init : () -> Url -> Key -> ( Model, Cmd Msg )
+init _ url key =
+    let
+        ( token, state, msg ) =
+            case receiveTokenAndState url of
+                TokenAndState tok stat ->
+                    ( Just tok, stat, Nothing )
+
+                TokenErrorAndState m stat ->
+                    ( Nothing, stat, Just m )
+
+                TokenDecodeError m ->
+                    ( Nothing, Nothing, Just m )
+
+                NoToken ->
+                    ( Nothing, Nothing, Nothing )
+    in
+    ( { key = key
+      , authorization = Nothing
+      , token = token
+      , state = state
+      , msg = msg
+      , replyType = "Token"
+      , reply =
+            case token of
+                Nothing ->
+                    Nothing
+
+                Just tok ->
+                    Just <| responseTokenEncoder tok
+      , redirectBackUri = locationToRedirectBackUri url
+      , authorizations = Dict.empty
+      , provider =
+            case state of
+                Just p ->
+                    p
+
+                Nothing ->
+                    "Gab"
+      , tokenAuthorization = Nothing
+      , api = Nothing
+      }
+    , Cmd.batch
+        [ Http.request <|
+            getAuthorizations ReceiveAuthorizations False "authorizations.json"
+        , Navigation.replaceUrl key "#"
+        ]
+    )
+
+
+getUser : Model -> ( Model, Cmd Msg )
+getUser model =
+    case model.token of
+        Nothing ->
+            ( { model
+                | msg = Just "You must login before getting user information."
+              }
+            , Cmd.none
+            )
+
+        Just token ->
+            case ( model.api, model.authorization ) of
+                ( Just api, Just auth ) ->
+                    let
+                        url =
+                            auth.apiUri ++ api.getUser
+
+                        req =
+                            Http.request
+                                { method = "GET"
+                                , headers = use token [ userAgentHeader ]
+                                , url = url
+                                , body = Http.emptyBody
+                                , expect = Http.expectJson ReceiveUser JD.value
+                                , timeout = Nothing
+                                , tracker = Nothing
+                                }
+                    in
+                    ( model, req )
+
+                _ ->
+                    ( { model | msg = Just "No known API." }
+                    , Cmd.none
+                    )
+
+
+lookupProvider : Model -> Model
+lookupProvider model =
+    let
+        authorization =
+            case Dict.get model.provider model.authorizations of
+                Nothing ->
+                    case List.head <| Dict.toList model.authorizations of
+                        Nothing ->
+                            Nothing
+
+                        Just ( _, auth ) ->
+                            Just auth
+
+                Just auth ->
+                    Just auth
+    in
+    case authorization of
+        Nothing ->
+            model
+
+        Just auth ->
+            case List.head <| Dict.toList auth.scopes of
+                Nothing ->
+                    model
+
+                Just ( _, scope ) ->
+                    let
+                        provider =
+                            auth.name
+
+                        api =
+                            Dict.get provider apis
+                    in
+                    { model
+                        | provider = provider
+                        , tokenAuthorization =
+                            Just
+                                { authorization = auth
+                                , scope = [ scope ]
+                                , state = Just model.provider
+                                , redirectBackUri = model.redirectBackUri
+                                }
+                        , api = api
+                        , authorization = authorization
+                    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        OnUrlRequest _ ->
+            ( model
+            , Cmd.none
+            )
+
+        OnUrlChange _ ->
+            ( model
+            , Cmd.none
+            )
+
+        ReceiveAuthorizations result ->
+            case result of
+                Err err ->
+                    ( { model | msg = Just <| Debug.toString err }
+                    , Cmd.none
+                    )
+
+                Ok authorizations ->
+                    let
+                        ( replyType, reply ) =
+                            case ( model.token, model.msg ) of
+                                ( Nothing, Nothing ) ->
+                                    ( "Authorizations"
+                                    , Just <|
+                                        authorizationsEncoder
+                                            authorizations
+                                    )
+
+                                _ ->
+                                    ( model.replyType, model.reply )
+                    in
+                    ( lookupProvider
+                        { model
+                            | authorizations =
+                                Dict.fromList <|
+                                    List.map (\a -> ( a.name, a )) authorizations
+                            , replyType = replyType
+                            , reply = reply
+                        }
+                    , Cmd.none
+                    )
+
+        ChangeProvider provider ->
+            ( lookupProvider
+                { model | provider = provider }
+            , Cmd.none
+            )
+
+        Login ->
+            case model.tokenAuthorization of
+                Nothing ->
+                    ( { model | msg = Just "No provider selected." }
+                    , Cmd.none
+                    )
+
+                Just authorization ->
+                    case authorize authorization of
+                        Nothing ->
+                            ( { model
+                                | msg = Just "Bad Uri in authorizations.json."
+                              }
+                            , Cmd.none
+                            )
+
+                        Just url ->
+                            ( model
+                            , Navigation.load <| Url.toString url
+                            )
+
+        GetUser ->
+            getUser model
+
+        ReceiveUser result ->
+            case result of
+                Err err ->
+                    ( { model
+                        | reply = Nothing
+                        , msg = Just <| Debug.toString err
+                      }
+                    , Cmd.none
+                    )
+
+                Ok reply ->
+                    ( { model
+                        | replyType = "API Response"
+                        , reply = Just reply
+                        , msg = Nothing
+                      }
+                    , Cmd.none
+                    )
+
+
+providerOption : String -> String -> Html Msg
+providerOption currentProvider provider =
+    option
+        [ value provider
+        , selected <| provider == currentProvider
+        ]
+        [ text provider ]
+
+
+providerSelect : Model -> Html Msg
+providerSelect model =
+    select [ onInput ChangeProvider ]
+        (Dict.toList model.authorizations
+            |> List.map Tuple.second
+            |> List.map .name
+            |> List.map (providerOption model.provider)
+        )
+
+
+view : Model -> Document Msg
+view model =
+    { title = "OAuthMiddleware Example"
+    , body =
+        [ div
+            [ style "margin-left" "3em"
+            ]
+            [ h2 [] [ text "OAuthMiddleware Example" ]
+            , p []
+                [ text "Provider: "
+                , providerSelect model
+                ]
+            , p []
+                [ button [ onClick Login ]
+                    [ text "Login" ]
+                , text " "
+                , button [ onClick GetUser ]
+                    [ text "Get User" ]
+                ]
+            , pre []
+                [ case ( model.msg, model.reply ) of
+                    ( Just msg, _ ) ->
+                        text <| Debug.toString msg
+
+                    ( _, Just reply ) ->
+                        text <| model.replyType ++ ":\n" ++ JE.encode 2 reply
+
+                    _ ->
+                        text "Nothing to report"
+                ]
+            ]
+        ]
+    }
